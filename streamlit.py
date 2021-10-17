f9ec4f18d421eac988b9eb52808fb3ab98f3849c
SkageLysgaard <31646924+SkageLysgaard@users.noreply.github.com>
2021-10-17 17:26:30 +0200

    Mer brukervennlig

--- streamlit.py
+++ streamlit.py
@@ -1,15 +1,17 @@
 import streamlit as sl
-import requests, json
-sl.sidebar.write("Menu")
-endpoint = sl.sidebar.selectbox("Endpoints", ['Assets', 'Events', 'Rarity'])
+import requests
+sl.sidebar.header("Menu")
+#made a selectbox so it's easy to scale
+endpoint = sl.sidebar.selectbox("Endpoints", ['Assets'])
 sl.header("Future of Fintech 2021 Kodekonkurranse")
 sl.subheader(f"OpenSea NFT API Explorer - {endpoint}")
 
 sl.sidebar.subheader("Filters")
+sl.sidebar.write("Collections to try: giphy, monkeybet")
 
 collection = sl.sidebar.text_input("Collections")
 owner = sl.sidebar.text_input("Owner")
-
+#just to make it easy to scale. but not neccesarry when my endpoint only contains one item
 if endpoint == 'Assets':
     params = {}
 
@@ -27,9 +29,9 @@ if endpoint == 'Assets':
             sl.write(asset['name'])
         else:
             sl.write(f"{asset['collection']['name']} #{asset['token_id']}")
-        if asset['image_url'].endswith('mp4'):
+        if asset['image_url'].endswith('mp4') or asset['image_url'].endswith('mov'):
             sl.video(asset['image_url'])
         else:
             sl.image(asset['image_url'])
 
-    sl.write(url.json())
+    
